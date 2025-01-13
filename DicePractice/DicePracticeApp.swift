fdde4464f81cd080afc57864200ec87ea9c3499a
Rovshan <soulofchinder.2@gmail.com>
2025-01-13 20:13:40 +0400

    Initial Commit

---
+++ DicePractice/DicePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicePracticeApp.swift
+//  DicePractice
+//
+//  Created by Rovshan Rasulov on 13.01.25.
+//
+
+import SwiftUI
+
+@main
+struct DicePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
